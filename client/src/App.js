ba47b13cddde6ad6544a28ac7a8769d95d62a693
Khrista K <khristakhwaja@gmail.com>
2023-04-11 14:00:28 -0700

    "birthday button"

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import Header from './components/PersonCard'
 function App() {
   return (
     <div className="App">
-      <Header lastName={"Doe"} firstName={"Jane"} age={"45"} hairColor={"Black"}/>
-      <Header lastName={"Smith"} firstName={"John"} age={"88"} hairColor={"Brown"}/>
-      <Header lastName={"Fillmore"} firstName={"Millard"} age={"50"} hairColor={"Brown"}/>
-      <Header lastName={"Smith"} firstName={"Maria"} age={"62"} hairColor={"Brown"}/>
+      <Header lastName={"Doe"} firstName={"Jane"} initialAge={45} hairColor={"Black"}/>
+      <Header lastName={"Smith"} firstName={"John"} initialAge={88} hairColor={"Brown"}/>
+      <Header lastName={"Fillmore"} firstName={"Millard"} initialAge={50} hairColor={"Brown"}/>
+      <Header lastName={"Smith"} firstName={"Maria"} initialAge={62} hairColor={"Brown"}/>
     </div>
   );
 }
--- client/src/components/PersonCard.js
+++ client/src/components/PersonCard.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = (props) => {
+    const [ bDay, setBDay ] = useState(props.initialAge);
     return (
         <div>
             <h1>
                 {props.lastName}, {props.firstName}
             </h1>
-            <h6>Age: {props.age}</h6>
+            <h6>Age: { bDay }</h6>
             <h6>Hair Color: {props.hairColor}</h6>
+            <button onClick= { () => setBDay(bDay + 1 )}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     );
 
